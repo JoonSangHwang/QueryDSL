09682f305b30c7bed6ac3102f63099e3398484e5
joonsang <gufrus@naver.com>
2021-02-04 16:46:48 +0900

    [2021.02.04] 예제 도메인 모델과 동작확인

---
+++ QueryDSL/src/main/java/com/joonsang/example/QueryDSL/entity/Team.java
@@ -0,0 +1,30 @@
+package com.joonsang.example.QueryDSL.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})      // @ToString은 가급적 내부 필드만(연관관계 없는 필드만)
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
